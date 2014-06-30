dfbd6d589f3d1d5d0feba0638b7b79d7d4b1c1fc
Flavio Silva <flv.alves@gmail.com>
2014-06-30 11:30:10 -0300

    retirando e colocando alguns &nbsp da pagina de contato

--- content/contato.php
+++ content/contato.php
@@ -1,10 +1,10 @@
 <?php if(empty($_POST)):?>
 <h5 class="text">Preencha os campos abaixo:</h5>
 <form action="" method="post" name="contato">
-    <label for="nome">Nome:&nbsp;&nbsp;</label><input type="text" name="nome" id="nome" required="required"/>
-    <label for="email">Email:&nbsp;&nbsp;</label><input type="email" name="email" id="email" required="required"/>
-    <label for="assunto">Assunto:&nbsp;&nbsp;</label><input type="text" name="assunto" id="assunto" required="required"/>
-    <label for="mensagem">Mensagem:&nbsp;&nbsp;</label>
+    <label for="nome">Nome:</label><input type="text" name="nome" id="nome" required="required"/>
+    <label for="email">Email:</label><input type="email" name="email" id="email" required="required"/>
+    <label for="assunto">Assunto:</label><input type="text" name="assunto" id="assunto" required="required"/>
+    <label for="mensagem">Mensagem:</label>
     <textarea style="width: 400px; height: 150px;" name="mensagem" id="mensagem" required="required"></textarea>
     <br />
     <input type="submit" name="enviar" class="btn">
@@ -12,7 +12,7 @@
 <?php else:?>
 <h5 class="text-info">Dados enviados com sucesso! Abaixo seguem os dados que você enviou:</h5>
 <p>Nome:&nbsp;&nbsp;<?= $_POST['nome']?> </p>
-<p>Email: <?= $_POST['email']?> </p>
-<p>Assunto: <?= $_POST['assunto']?> </p>
-<p>Mensagem: <?= $_POST['mensagem']?> </p>
+<p>Email:&nbsp;&nbsp;<?= $_POST['email']?> </p>
+<p>Assunto:&nbsp;&nbsp;<?= $_POST['assunto']?> </p>
+<p>Mensagem:&nbsp;&nbsp;<?= $_POST['mensagem']?> </p>
 <?php endif;?>
