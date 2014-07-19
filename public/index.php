711e269306f19000cde43c079affaa26a9a2d329
Flavio Silva <flv.alves@gmail.com>
2014-07-19 14:12:47 -0300

    Modificando o index para trabalhar com duas funcoes para obter conteudo

--- public/index.php
+++ public/index.php
@@ -5,41 +5,41 @@ require_once __DIR__ . '/../conexao.php';
 $route = preg_filter('/^\//', '', $_SERVER['REQUEST_URI']);
 $route = ($route == null)?('home'):($route);
 
+$validRoutes = ['home', 'empresa','produtos', 'servicos', 'contato', 'pesquisar'];
 
-$validRoutes = ['home', 'empresa', ]
+function getContent($db, callable $checkRoute)
+{
+    $page = call_user_func($checkRoute);
+    if($page) {
 
-// Funcao para validar as rotas e verificar se o arquivo requisitado existe
-function checkRoute ($route, array $validRoutes) {
-    if(in_array($route, $validRoutes) && file_exists(__DIR__ . '/../content/' . $route . '.php')) {
-        return true;
-    } else {
-        return false;
-    }
-};
-
-if(checkRoute($route, $validRoutes) === true) {
-    //O unico conteudo com require, pois se trata de um formulario
-    if(in_array($route, array('contato', 'pesquisar'))) {
-        require_once '/../content/' . $route . '.php';
-    } else {
-        require __DIR__ . '/../fixtures.php';
-        $sql = 'select * from paginas where nome=:nome';
-        $stmt = $db->prepare($sql);
-        $stmt->bindValue(':nome', $route);
+        $stmt = $db->prepare('select * from paginas where nome=:page');
+        $stmt->bindValue(':page', $page);
         $stmt->execute();
-        $pagina = $stmt->fetch(\PDO::FETCH_ASSOC);
-        echo $pagina['conteudo'];
+        return $stmt->fetch(PDO::FETCH_ASSOC)['conteudo'];
+
+    } else {
+        http_response_code(404);
+        return 'Erro 404: Página não existente';
     }
-    
-} else {
-    http_response_code(404);
-    echo 'Erro 404: Página não existente';
 }
 
 //Colando o cabeçalho do documento e o menu
 require_once __DIR__ . '/../layout/header.php';
 
 //Conteudo
+echo getContent($db, function() use($route, $validRoutes){
+    if(in_array($route, $validRoutes)) {
+        return $route;
+    } else {
+        return false;
+    }
+});
+
+//Se a pagina tiver html, neste caso paginas contato e pesquisar
+if(in_array($route, array('contato', 'pesquisar')) && file_exists(__DIR__ . "/../content/{$route}.php")) {
+
+    require_once __DIR__ . "/../content/{$route}.php";
+}
 
 //aqui fica o rodape
 require_once __DIR__ . '/../layout/footer.php';
