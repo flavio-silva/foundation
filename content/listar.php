5e109d444d391e6473937a9cbcf32f3a5271645f
Flavio Silva <flv.alves@gmail.com>
2014-07-23 19:52:28 -0300

    - criando uma pagina para validar a autenticação - criando a listagem das paginas para gerenciamento do conteúdo

--- content/listar.php
+++ content/listar.php
@@ -1,6 +1,26 @@
+<?php
+    require_once __DIR__ . '/../authentication.php';
+    require_once __DIR__ . '/../conexao.php';
+
+/* @var $db PDO*/
+$stmt = $db->query('select * from paginas');
+$paginas = $stmt->fetchAll(PDO::FETCH_ASSOC);
+?>
+<h4 class="text-center">Listagem de páginas</h4>
+<br />
 <table class="table">
 
     <th>Página</th>
     <th>Ação</th>
+<?php foreach($paginas as $pagina):?>
+    <tr>
+       <td><?= $pagina['nome']?></td>
+        <td><a href="?acao=editar&id=<?=$pagina['id']?>">Editar Conteúdo</a>
+        &nbsp;| &nbsp;
+        <a href="?acao=apagar&id=<?=$pagina['id']?>">Apagar Conteúdo</a>
+        </td>
 
+
+    </tr>
+    <?php endforeach;?>
 </table>
